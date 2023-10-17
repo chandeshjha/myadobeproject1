16228ce8e90154e7f1615d0919b39aa1c96cd535
Chandesh Jha <chandeshj@adobe.com>
2023-10-17 11:43:48 +0530

    changes for repo

---
+++ ui.frontend/src/components/Weather/Weather.js
@@ -0,0 +1,48 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~ Copyright 2018 Adobe Systems Incorporated
+ ~
+ ~ Licensed under the Apache License, Version 2.0 (the "License");
+ ~ you may not use this file except in compliance with the License.
+ ~ You may obtain a copy of the License at
+ ~
+ ~     http://www.apache.org/licenses/LICENSE-2.0
+ ~
+ ~ Unless required by applicable law or agreed to in writing, software
+ ~ distributed under the License is distributed on an "AS IS" BASIS,
+ ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~ See the License for the specific language governing permissions and
+ ~ limitations under the License.
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+ 
+import React, {Component} from 'react';
+import ReactWeather from 'react-open-weather';
+import {MapTo} from '@adobe/aem-react-editable-components';
+
+require('./Weather.css');
+
+const WeatherEditConfig = {
+
+    emptyLabel: 'Weather',
+
+    isEmpty: function() {
+        return !this.props || !this.props.cq_model || !this.props.cq_model.city || this.props.cq_model.city.trim().length < 1;
+    }
+};
+
+class Weather extends Component {
+
+    render() {
+        let apiKey = "12345678901234567890";
+        let city;
+
+        if (this.props.cq_model) {
+            city = this.props.cq_model.city;
+            return <ReactWeather key={'react-weather' + Date.now()} forecast="today" apikey={apiKey} type="city" city={city} />
+        }
+
+        return null;
+    }
+}
+
+
+MapTo('ckj-spa/components/weather')(Weather, WeatherEditConfig);
